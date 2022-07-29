10e3c8012b6b246a262298b338727ff4785ad299
prabinrajkp <prabinraj.kp18@gmail.com>
2022-07-29 19:35:54 +0530

    minor changes

--- data.py
+++ data.py
@@ -5,16 +5,17 @@ import streamlit as st
 import os
 
 
-j=st.secrets['js']
-res = json.loads(j)
 
-with open('data.json', 'w') as f:
-    json.dump(res, f)
     
 print('json file created')
 
 
 def athletics():
+	j=st.secrets['js']
+	res = json.loads(j)
+	with open('data.json', 'w') as f:
+		json.dump(res, f)
+
 	gc = gs.service_account(filename='data.json')
 	os.remove('data.json')
 	sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1EWuTD-8ATgSxEcuCSVVoYGQ4WIrSpwvlU3u_-Z7qxbo/edit?usp=sharing')
