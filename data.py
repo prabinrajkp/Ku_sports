2c9d0f0461d4ae9a360649f2e726c911c6843edc
prabinrajkp <prabinraj.kp18@gmail.com>
2022-07-29 18:30:24 +0530

    secrets managed

--- data.py
+++ data.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import gspread as gs
+import json
+import streamlit as st
+import os
+
+
+j=st.secrets['js']
+res = json.loads(j)
+
+with open('data.json', 'w') as f:
+    json.dump(res, f)
+
 
 def athletics():
-	gc = gs.service_account(filename='horizontal-ward-317609-7b13c9012800.json')
+	gc = gs.service_account(filename='data.json')
+	os.remove('data.json')
 	sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1EWuTD-8ATgSxEcuCSVVoYGQ4WIrSpwvlU3u_-Z7qxbo/edit?usp=sharing')
 	ws = sh.worksheet('Sheet1')
 	df = pd.DataFrame(ws.get_all_records())
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from data import athletics
 import dataframe_image as dfi
 #import gspread as gs
+import json
 
 st.markdown('# Campus olympics')
 
@@ -20,44 +21,69 @@ with col2:
 
 st.write('---')
 
-tab1, tab2,tab3 = st.tabs(["Event Wise Result","Point Table - Faculty-Wise","Point Table - Department_wise"])
+tab1, tab2,tab3 = st.tabs(["Event Wise Result","Leader Board","Point Table - Department_wise"])
 
 df=athletics()
 ev=df['Event'].unique()
 
 with tab1:
 
+	c1,c2=st.columns(2)
+	#with c1:
 	option = st.selectbox('Event result',ev)
 
 	st.write('Results For ', option)
 
 	ev=df[df['Event']==option]
 	ev=ev[['Name','Faculty','Department','Position']]
-	
-	bd=ev.style.set_properties(**{'background-color': 'White',
-                           'color': 'Red',
-                           'border-color': 'white'}).hide_index().set_caption(str(option)+' result')
-	
+		
+		
+	bd=ev.style.set_properties(**{'background-color': 'black',
+		                       'color': 'white',
+		                       'border-color': 'Red'}).hide_index().set_caption(str(option)+' result')
+		
 	st.dataframe(ev)
-	dfi.export(bd, 'Result.png')
-	
-	with open("result.png", "rb") as file:
-		btn = st.download_button(
-             label="Download Result",
-             data=file,
-             file_name="result.png",
-             mime="image/png"
-           )
+	dfi.export(ev, 'Result.png')
+		
+	with open("Result.png", "rb") as file: btn = st.download_button(
+		         label="Download Result",
+		         data=file,
+		         file_name="result.png",
+		         mime="image/png"
+		       )
+		       
+	#with c2:
+	st.markdown('###### Leaderboard - Individual')
+	dpt=df[df['Department']!='']
+	dpt=dpt.groupby(['Name']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False).head(10)
+	st.dataframe(dpt)
+		
 
 	
 with tab2:
+	c1,c2=st.columns(2)
 	
-	st.write('coming soon')
-	st.dataframe(df.groupby(['Faculty']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False))
+	with c1:
+	
+		
+		st.markdown('###### Leaderboard - Faculty wise')
+		st.dataframe(df.groupby(['Faculty']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False))
+	
+	with c2:
+		st.markdown('###### Leaderboard - Department wise')
+		dpt=df.groupby(['Department']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False)
+		dpt=dpt.head(10)
+		st.dataframe(dpt[dpt['Department']!=''])
+
+	
+	
+
 
 with tab3:
-	dpt=df.groupby(['Department']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False)
-	st.dataframe(dpt[dpt['Department']!=''])
+	
+	
+	
+	st.write('ssbsbbs')
 
 #st.dataframe(df)
 	
