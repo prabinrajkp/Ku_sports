bcead0f1de9c815a10a3a03ccf3482ca40cf3778
prabinrajkp <prabinraj.kp18@gmail.com>
2022-08-02 19:28:59 +0530

    data changes

--- data.py
+++ data.py
@@ -18,8 +18,8 @@ def athletics():
 
 	gc = gs.service_account(filename='data.json')
 	os.remove('data.json')
-	sh = gc.open_by_url(st.secrets['result'])
-	ws = sh.worksheet('Sheet1')
+	sh = gc.open_by_url(st.secrets['reg'])
+	ws = sh.worksheet('Sheet2')
 	df = pd.DataFrame(ws.get_all_records())
 	
 	return df
