335531534e34dfba1a3da257e35a961982cfc4e4
prabinrajkp <prabinraj.kp18@gmail.com>
2022-08-02 20:40:43 +0530

    minor update

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ else:
 with st.sidebar.expander("Developers"):
  	st.markdown('#### [Prabin Raj K P](https://www.linkedin.com/in/prabinrajkp18/)')
  	st.markdown('#### [Vijay V Venkitesh](https://www.linkedin.com/in/vijay-v-venkitesh-673177204/)')
- 	st.write('##### MSc Data Science \n Department of Future Studies')
+ 	st.write('##### MSc Data Science \n Department of Futures Studies')
  	
      #st.image("https://static.streamlit.io/examples/dice.jpg")
 			
